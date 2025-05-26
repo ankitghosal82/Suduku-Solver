3cf82554a81eff2692cea40b73046b0b6ab37034
Ankit Ghosal <ankitghosal82@gmail.com>
2025-05-26 20:10:38 +0530

    Update suduku.py

--- suduku.py
+++ suduku.py
@@ -1,66 +1,78 @@
-def is_valid(board, row, col, num):
-    # Check if the number is in the row
-    if num in board[row]:
-        return False
+import random
+import copy
 
-    # Check if the number is in the column
-    for i in range(9):
-        if board[i][col] == num:
-            return False
+class SudokuSolver:
+    def __init__(self, board):
+        self.board = board
+        self.steps = []
+        self.recursive_calls = 0
+        self.backtracks = 0
 
-    # Check in the 3x3 subgrid
-    start_row = (row // 3) * 3
-    start_col = (col // 3) * 3
-
-    for i in range(3):
-        for j in range(3):
-            if board[start_row + i][start_col + j] == num:
+    def is_valid(self, num, row, col):
+        for i in range(9):
+            if self.board[row][i] == num or self.board[i][col] == num:
                 return False
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if self.board[start_row + i][start_col + j] == num:
+                    return False
+        return True
 
-    return True
+    def solve(self):
+        self.recursive_calls += 1
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    for num in range(1, 10):
+                        if self.is_valid(num, i, j):
+                            self.board[i][j] = num
+                            self.steps.append(copy.deepcopy(self.board))
+                            if self.solve():
+                                return True
+                            self.board[i][j] = 0
+                            self.backtracks += 1
+                    return False
+        return True
 
-def find_empty(board):
+def generate_full_board():
+    board = [[0 for _ in range(9)] for _ in range(9)]
+    solver = SudokuSolver(board)
+    nums = list(range(1, 10))
     for i in range(9):
         for j in range(9):
+            random.shuffle(nums)
+            for num in nums:
+                if solver.is_valid(num, i, j):
+                    board[i][j] = num
+                    if SudokuSolver(copy.deepcopy(board)).solve():
+                        break
+                    board[i][j] = 0
             if board[i][j] == 0:
-                return (i, j)
-    return None
+                return generate_full_board()
+    return board
 
-def solve_sudoku(board):
-    empty = find_empty(board)
-    if not empty:
-        return True  # Solved
+def remove_cells(board, difficulty="easy"):
+    difficulty_map = {
+        "easy": 35,
+        "medium": 45,
+        "hard": 55
+    }
+    cells_to_remove = difficulty_map.get(difficulty, 35)
+    puzzle = copy.deepcopy(board)
+    removed = 0
+    while removed < cells_to_remove:
+        i = random.randint(0, 8)
+        j = random.randint(0, 8)
+        if puzzle[i][j] != 0:
+            puzzle[i][j] = 0
+            removed += 1
+    return puzzle
 
-    row, col = empty
-
-    for num in range(1, 10):
-        if is_valid(board, row, col, num):
-            board[row][col] = num
-
-            if solve_sudoku(board):
-                return True
-
-            # Backtrack
-            board[row][col] = 0
-
-    return False
-
-# Example usage:
-if __name__ == "__main__":
-    sudoku_board = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]
-
-    if solve_sudoku(sudoku_board):
-        for row in sudoku_board:
-            print(row)
+def estimate_difficulty(steps_count):
+    if steps_count < 50:
+        return "Easy"
+    elif steps_count < 150:
+        return "Medium"
     else:
-        print("No solution exists.")
+        return "Hard"
